64e2bc399609118612c3742049f4df64ee0e06a1
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2021-09-29 19:41:28 +0530

    comment using shell script

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
+var c = document.getElementById('c') ;
+var r = document.getElementById('r') ;
+var g = document.getElementById('g') ;
+var b = document.getElementById('b') ;
+var a = document.getElementById('a') ;
 function refreshColor() {
-	var r = document.getElementById('r').value ;
-	var g = document.getElementById('g').value ;
-	var b = document.getElementById('b').value ;
-	var a = document.getElementById('a').value / 100 ;
-	var c = 'rgba( ' + r + ', ' + g + ', ' + b + ', ' + a + ' )' ;
-	document.body.style.background = c ;
-	document.getElementById('c').value = c ;
-	document.getElementById('a').style.background = 'linear-gradient(90deg,rgba(0,0,0,0),' + 'rgba(' + r + ',' + g + ',' + b + ')' ;
+	var color = r.value + ', ' +
+			g.value + ', ' +
+			b.value ;		
+	c.style.background =  'rgba(' +
+		color +  ', ' +
+		a.value / 100 + 
+	' )' ;
+	c.value = c.style.background ;
+	a.style.background = 'linear-gradient(to right,' +' rgba(0, 0, 0 )'+ ',' + 'rgba(' + color + ' )' ;
 }
-document.getElementById('r').addEventListener('input', refreshColor) ;
-document.getElementById('g').addEventListener('input', refreshColor) ;
-document.getElementById('b').addEventListener('input', refreshColor) ;
-document.getElementById('a').addEventListener('input', refreshColor) ;
+r.addEventListener('input', refreshColor) ;
+g.addEventListener('input', refreshColor) ;
+b.addEventListener('input', refreshColor) ;
+a.addEventListener('input', refreshColor) ;
