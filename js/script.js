529451f4447b0a3a3b95506e4223665c473c9962
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2021-09-30 13:18:12 +0530

    view with set

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ var b = document.getElementById('b') ;
 var a = document.getElementById('a') ;
 function refreshColor() {
 	var color = r.value + ', ' +
-			g.value + ', ' +
-			b.value ;		
+		g.value + ', ' +
+		b.value ;		
 	c.style.background =  'rgba(' +
 		color +  ', ' +
 		a.value / 100 + 
 	' )' ;
 	c.value = c.style.background ;
-	a.style.background = 'linear-gradient(to right,' +' rgba(0, 0, 0 )'+ ',' + 'rgba(' + color + ' )' ;
+	a.style.background = 'linear-gradient(to right,' + ' rgba(0, 0, 0 )' + ',' + 'rgba(' + color + ' )' ;
 }
 r.addEventListener('input', refreshColor) ;
 g.addEventListener('input', refreshColor) ;
